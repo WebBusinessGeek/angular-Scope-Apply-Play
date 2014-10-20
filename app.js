b49df7b2787fbcca2d2f2c0c787a8a39f10b2615
Kevin Washington <kevw12188@gmail.com>
2014-10-20 16:51:52 -0400

    example complete

---
+++ app.js
@@ -0,0 +1,38 @@
+var app = angular.module('app', [])
+
+
+app.directive('watchedFalse', function(){
+	return{
+		restrict: 'E', 
+		template: '<button class="btn btn-success">I was clicked {{counter1}} times</button>',
+		controller: function($scope){
+			$scope.counter1 = 0;
+		},
+		link: function(scope, element, attrs){
+			element.bind('click', function(){
+				scope.counter1++;
+			});
+		}
+	}
+
+
+})
+
+
+app.directive('watchedTrue', function(){
+	return{
+		restrict: 'E', 
+		template: '<button class="btn btn-primary">I was clicked {{counter2}} times</button>',
+		controller: function($scope){
+			$scope.counter2 = 0;
+		},
+		link: function(scope, element, attrs){
+			element.bind('click', function(){
+				scope.$apply(function(){
+					scope.counter2++;
+				});
+			});
+		}
+	}
+
+})
